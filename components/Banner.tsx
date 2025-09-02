cb4ee7682926e8d5dc842ed373d56d1850291df3
Hoàng ✓ <86339218+HoangNoPro@users.noreply.github.com>
2025-09-02 18:23:36 +0700

    Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -13,16 +13,16 @@ const Banner: React.FC<{}> = () => {
             src="/Me.png"
             height={250}
             width={250}
-            alt="Ibrahim Memon"
+            alt="Chaos"
           />
         </div>
 
         <div className="flex flex-col gap-6 mt-10 cursor-pointer animate-bounce tracking-tighter text-7xl font-semibold text-white max-w-[600px] w-auto h-auto">
-          Ibrahim Memon!
+          Hi, I'm !
         </div>
         <div className="flex justify-center">
           <p className="text-2xl font-medium tracking-tighter  text-gray-300 max-w-[600px]">
-            I do Code &{" "}
+            I play  &{" "}
             <span className="text-transparent font-semibold  bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
               Chill
             </span>
@@ -31,17 +31,16 @@ const Banner: React.FC<{}> = () => {
             src="/popcorn.gif"
             height={50}
             width={50}
-            alt="Ibrahim Memon"
+            alt="Chaos"
           />
         </div>
 
         <p className="text-md text-gray-200 my-5 max-w-[600px]">
-          Passionate Software Engineer with a focus on React Native development,
-          dedicated to crafting elegant and user-friendly mobile applications.
+          Passionate about gaming, studying and listening to music.
         </p>
         <div className="text-md flex justify-center">
           <button
-            onClick={() => window.open("mailto:ibrahimmemon930@gmail.com")}
+            onClick={() => window.open("mailto:hoang123091@gmail.com")}
             className=" z-[1]     padding-20  hover:bg-white rounded-3xl  text-white font-semibold hover:text-black py-3 px-10  border-[0.1px] border-white hover:border-transparent "
           >
             Contact Me
